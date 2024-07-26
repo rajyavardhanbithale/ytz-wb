ec871fb5507187899715ccd7d47517d7e577fb5a
vedant <vedant@ssipmt.com>
2024-07-26 12:46:16 +0530

    navigation menu

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+} from "@/components/ui/navigation-menu"
 import { IoSearchSharp } from "react-icons/io5";
 import { IoIosArrowDropdown } from "react-icons/io";
 import { VscAccount } from "react-icons/vsc";
@@ -18,7 +20,7 @@ function Navbar() {
         <img className='mr-10' src="" alt="web logo" />
       </div>
       <div className='hidden md:block w-[35%] h-[60%] '>
-        <div className='w-full h-full rounded-md bg-blue-50 flex justify-between items-center  border-2 border-gray-100'>
+        <div className='w-full h-full rounded-md bg-blue-50 flex justify-between items-center  border-2 border-gray-100 '>
           <IoSearchSharp className='w-[5%] h-[60%] opacity-40' />
           <input
             className='w-[95%] h-full bg-blue-50 border-none rounded-md  px-4 outline-none appearance-none '
@@ -32,23 +34,25 @@ function Navbar() {
       </div>
 
       <div className='mx-10 w-fit md:w-[7%] h-[60%] bg-white px-2 py-1 rounded-md border-none  hover:bg-blue-600 flex flex-col items-center justify-center ' >
-        <DropdownMenu >
-          <DropdownMenuTrigger className='text-lg flex justify-around items-center h-full w-full outline-none hover:text-white '>
-            <VscAccount className='h-full'/> Login <IoIosArrowDropdown />
-          </DropdownMenuTrigger>
-          <DropdownMenuContent >
-            <DropdownMenuLabel >
-              My Account 
-            </DropdownMenuLabel>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem>
-              Profile
-            </DropdownMenuItem>
-            <DropdownMenuItem>Billing</DropdownMenuItem>
-            <DropdownMenuItem>Team</DropdownMenuItem>
-            <DropdownMenuItem>Subscription</DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
+        {/* <VscAccount className='h-full font-bold' /> Login <IoIosArrowDropdown /> */}
+        <NavigationMenu className='w-full h-full  flex justify-around items-center'>
+          <NavigationMenuList>
+            <NavigationMenuItem>
+
+              <NavigationMenuTrigger className='w-full flex justify-around items-center hover:text-white text-lg'>
+                <VscAccount className='h-full font-bold text-xl mx-1' /> Login
+              </NavigationMenuTrigger>
+
+              <NavigationMenuContent>
+                <NavigationMenuLink>
+                  Link
+                </NavigationMenuLink>
+              </NavigationMenuContent>
+
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>
+
       </div>
 
 
