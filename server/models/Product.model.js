07329b2d89ef1504385f09ddac4ffb72f25fcfbe
vedant <vedant@ssipmt.com>
2024-08-07 19:25:06 +0530

    product Schema added

---
+++ server/models/Product.model.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+    id: {
+        type: String,
+        required: true
+    },
+    productName: {
+        type: String,
+        required: true
+    },
+    sellerName: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    price: { 
+        type: Number,
+        required: true 
+    },
+    discountPrice: { 
+        type: Number 
+    },
+    totalAvailable: { 
+        type: Number,
+        required: true 
+    },
+    imageURL: {
+        public_id :{
+            type : 'String'
+        },
+        secure_url: {
+            type : 'String'
+        }
+    },
+    category: [String],
+    tags: [String],
+    inStock: { 
+        type: Boolean, 
+        default: true 
+    }
+},{
+    timestamps:true,
+});
+
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = Product;
