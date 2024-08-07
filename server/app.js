12844d36f65c5faf931a641dc541e30ab30c3cfe
vedant <vedant@ssipmt.com>
2024-08-07 13:55:34 +0530

    new server

---
+++ server/app.js
@@ -0,0 +1,54 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import morgan from 'morgan';
+import cookieParser from 'cookie-parser';
+import path from 'path'; // Import the path module
+import errorMiddleware from './middlewares/error.middleware.js';
+
+// Load environment variables
+dotenv.config({
+  path: './.env'
+});
+
+const app = express();
+
+// Middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors({
+  origin: [process.env.FRONTEND_URL],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS']
+}));
+app.use(morgan('dev'));
+app.use(cookieParser());
+
+// Import all routes
+// import userRoutes from './routes/user.routes.js';
+
+// app.use('/api/v1/user', userRoutes);
+
+
+
+// Server Status Check Route
+app.get('/ping', (_req, res) => {
+  res.send('Pong');
+});
+
+
+// Default catch all route - 404
+app.all('*', (_req, res) => {
+  res.status(404).send('OOPS!!! 404 Page Not Found');
+});
+
+// Custom error handling middleware
+app.use(errorMiddleware);
+
+// Error handling for unhandled promise rejections
+process.on('unhandledRejection', (err) => {
+  console.error('Unhandled Promise Rejection:', err);
+  process.exit(1);
+});
+
+export default app;
